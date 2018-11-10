42da22bd6ceacf6c2d3bd933b4b3b62c9b102d61
aras armani <aras.armani2014@gmail.com>
2018-11-10 18:50:36 +0300

    listign animated in fixed

--- src/container/dashboard/Listing.js
+++ src/container/dashboard/Listing.js
@@ -1,11 +1,26 @@
 import React,{Component} from 'react';
+import { NavLink } from 'react-router-dom'
 import './style.css';
+
+import IconAdd from '../../assets/icons/add.svg';
+import IconSearch from '../../assets/icons/search.svg';
+
 
 
 class ListingComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
+    }
+
+    enterPressed(event) {
+        var code = event.keyCode || event.which;
+        if(code === 13) { 
+            alert("key pressed!")
+        }
+    }
+    _callSearch(){
+        alert("this is called from searc ")
     }
     render() { 
         return ( 
@@ -17,17 +32,21 @@ class ListingComponent extends Component {
                         <div className="search-container">
                             <div className="search-box">
                                 <div className="search-input">
-                                    <input type="text" name="search" placeholder="search . . . " />
+                                    <input type="text" name="search" placeholder="search . . . " onKeyPress={this.enterPressed} />
                                 </div>
-                                <div className="search-button">
-                                    <span>P</span>
-                                </div>
+                                <button className="search-button" type="submit" onClick={this._callSearch} >
+                                    <span> <img src={IconSearch} className="Serch-Icon" alt="Search Contact"/> </span>
+                                </button>
                             </div>
                         </div>
                         <div className="menubutton-container">
-                            <div className="button-box">
-                            <span>+</span> <p> Add New Contact</p>
-                            </div>
+                            <NavLink to="/login" >
+                                <div className="button-box">
+                                    <div>
+                                    <img src={IconAdd} className="Add-Icon" alt="Add New"/>    
+                                    </div> <p> Add New Contact</p>
+                                </div>
+                            </NavLink>
                         </div>
                         
                     </div>

75edfdce7bce63605012b49a6e6facc1629d481e
aras armani <aras.armani2014@gmail.com>
2018-11-20 13:53:46 +0300

    creare base url

---
+++ src/baseurl.js
@@ -0,0 +1,29 @@
+ //
+ // Create Base URL 'API'
+ //
+ 
+
+ const url ='http://localhost:5050' ;
+ const versioan ='v1' ;
+ const api ='api' ;
+
+ const baseURL = url + '/' + api + '/' + versioan + '/'
+
+ export default {
+     url, 
+     versioan, 
+     api,
+
+     baseURL
+    };
+
+
+
+
+     ////////////////////////////////////////////////////////////                                           
+     //                                                         |
+     //                                                         |
+     // somthing like this : http://localhost:5050/api/v1/      |
+     //                                                         |
+     //                                                         |
+     ///////////////////////////////////////////////////////////
--- src/container/auth/Register.js
+++ src/container/auth/Register.js
@@ -2,12 +2,43 @@ import React, { Component} from 'react';
 import { NavLink } from 'react-router-dom'
 import './style.css';
 
+import base from '../../baseurl';
+
+ 
 
 class RegisterComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            username:'',
+            email:'',
+            password:''
+         }
+         this.handleChange = this.handleChange.bind(this);
+         this.handleSubmit = this.handleSubmit.bind(this);
+
     }
+
+
+    handleChange(e) {
+        
+        if(e.target.name === 'email')
+            this.setState({email: e.target.value});
+        else if(e.target.name === 'username')
+            this.setState({username: e.target.value});
+        else if(e.target.name === 'password')
+            this.setState({password: e.target.value});
+   
+      }
+
+      handleSubmit(event) {
+        // alert('A name was submitted: ' + this.state.email + " name: " + this.state.username + " pass: " + this.state.password);
+console.log(base.baseURL)
+        
+        console.log(this.state.username)
+        event.preventDefault();
+      }
+
     render() { 
         return ( 
             <div className="container-fluid">
@@ -19,21 +50,21 @@ class RegisterComponent extends Component {
                       <hr />
                   </div>
                   <div className="auth-form">
-                      <form className="form">
+                      <form  onSubmit={this.handleSubmit} className="form">
                            
-                              <label for="email">Email</label>
-                              <input type="text" name="email" />
+                              <label htmlFor="email">Email</label>
+                              <input type="email" value={this.state.email} onChange={this.handleChange}  name="email" />
 
-                              <label for="username">Username</label>
-                              <input type="text" name="username" />
+                              <label htmlFor="username">Username</label>
+                              <input type="text" value={this.state.username} onChange={this.handleChange} name="username"  />
 
-                              <label for="password">Password</label>
-                              <input type="password" maxLength="20" name="password" />
+                              <label htmlFor="password">Password</label>
+                              <input type="password" value={this.state.password} onChange={this.handleChange}  maxLength="20" name="password" />
                           
-                          <input type="submit" value="Sign Up" />
+                          <input type="submit" className="blue-color"  value="Sign Up" />
                       </form>
                   </div>
-                  <div className="auth-footer">
+                  <div className="auth-footer ">
                       <p>Do you have account, Please <span> <NavLink className="sub-navlink" to="/login" >Login </NavLink> in your account. </span></p> 
 
                   </div>

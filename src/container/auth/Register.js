7071d8c28cb91f1029c8a9ef83413fa5140c8e70
aras armani <aras.armani2014@gmail.com>
2018-11-29 17:01:36 +0300

    change register validation

--- src/container/auth/Register.js
+++ src/container/auth/Register.js
@@ -11,15 +11,19 @@ import base from '../../baseurl';
     usernameError:'',
     emailError:'',
     passwordError:'',
-    isError:'Your account has been Created successfully.\n You can now login in your account.',
+    isError:'',
     isLoading:false,
     validate:false,
+    statusCode:0
+ 
  }
 
 class RegisterComponent extends Component {
     constructor(props) {
         super(props);
-        this.state =  initialState;
+         this.state =  initialState
+
+
          this.handleChange = this.handleChange.bind(this);
          this.handleSubmit = this.handleSubmit.bind(this);
 
@@ -35,44 +39,27 @@ class RegisterComponent extends Component {
             console.log("data set on key press!")
       }
 
-      handleonFocus = async(e) =>{
-        if(this.validateEmail(this.state.email) === false && this.state.email.length >5) 
-        {
-            this.setState({emailError : "Your email address is invalid. Please enter a valid address."})
-            this.setState({validate : false})
-        }
-        else
-        {  
-            this.setState({emailError : ''})
-            this.setState({validate : true})
+      validate = async(e) =>{
+
+        if(this.validateEmail(this.state.email) === false && this.state.email.length >2) 
+            this.setState({validate : false, emailError : "Your email address is invalid. Please enter a valid address."})
+        else  
+            this.setState({emailError : '',validate : true}) 
         
-        }
 
         if(this.state.username.length < 5 && this.state.username !== '')
-        {
-            this.setState({usernameError : "Usernames must be at least 5 characters long!"})
-            this.setState({validate : false})
-        }
-        else
-        {  
-            this.setState({usernameError : ''})
-            this.setState({validate : true})
+            this.setState({validate : false, usernameError : "Usernames must be at least 5 characters long!"})
+        else  
+            this.setState({usernameError : '', validate : true}) 
         
-        }
 
         if(this.state.password.length < 5 && this.state.password !== '')
-        {
-            this.setState({passwordError : "Password must be at least 5 characters long!"})
-            this.setState({validate : false})
-        }
-        else
-        {  
-            this.setState({passwordError : ''})
-            this.setState({validate : true})
+            this.setState({validate : false, passwordError : "Password must be at least 5 characters long!"})
+        else  
+            this.setState({passwordError : '', validate : true}) 
         
-        }
 
-        return true;
+        return false;
       }
 
    
@@ -107,38 +94,23 @@ class RegisterComponent extends Component {
 
 
 
-         // this.setState({emailError:'Your email address is invalid. Please enter a valid address.'})
-
-
-      validate = () =>{
-           let usernameError=''
-           let emailError=''
-           let passwordError='';
-
-           if(!this.state.email.includes('@')){
-                emailError = 'Invalid Email!'
-           }
-
-
-           if(emailError){
-               this.setState({emailError});
-               return false;
-           }
-
-           return true
-    }
-      handleSubmit(event) {
-        // alert('A name was submitted: ' + this.state.email + " name: " + this.state.username + " pass: " + this.state.password);
+ 
+      handleSubmit= async(event) => {
+        
         event.preventDefault();
-        console.log(base.baseURL) 
+        
+        console.log(this.state.validate)
+        if(this.state.validate === true) {
+
+        console.log(this.state)
+        // console.log(base.baseURL) 
         const formdata = new FormData();
         formdata.append('email', this.state.email);
         formdata.append('username', this.state.username);
         formdata.append('password', this.state.password);
 
 
-        if(this.state.validate === true)
-        {
+      
         const data ={
             method: 'POST',
             headers:{
@@ -148,16 +120,31 @@ class RegisterComponent extends Component {
         }
         const url = base.baseURL + 'auth/register';
        
-        fetch(url,data)
-        .then(response => response.json())
-        .then(datas => console.log(datas))
+       await fetch(url,data)
+        .then(response => {
+            response.json() 
+            console.log(response)
+            console.log(response.status)
+            this.setState({statusCode:response.status})
+            console.log(this.state.statusCode)
+        })
         .catch(error => console.log(error));
-    }
-        const isValid = this.validate();
-        if(isValid){
-        console.log(this.state)
-        // clear forms
-        this.setState(initialState)
+
+        console.log(this.state.statusCode + "wwww")
+            if(this.state.statusCode === 201){
+            // clear forms
+            
+             this.setState({ isSuccess:'Your account has been Created successfully.\n You can now login in your account.'}) 
+             this.setState(initialState)
+             console.log("sucsessful")
+            }
+            else if(this.state.statusCode === 401){
+                this.setState({ isError:'Email address is alredy!'}) 
+            }
+        }
+        else{
+            console.log("somethign is worng")
+            
         }
        
       }
@@ -171,7 +158,7 @@ class RegisterComponent extends Component {
 
     
 
-    render() { 
+    render() {  
         return ( 
             <div className="container-fluid">
               
@@ -183,25 +170,48 @@ class RegisterComponent extends Component {
                   </div>
                   <div className="auth-form">
 
-                  {!this.state.isError ? '' :
-                    <p className="success-box">{this.state.isError}</p>
+                    {!this.state.isError ? '' :
+                        <p className="error-box">{this.state.isError}</p>
+                    }
+
+                     {!this.state.isSuccess ? '' :
+                        <p className="success-box">{this.state.isSuccess}</p>
                     }
 
 
                       <form  onSubmit={this.handleSubmit} className="form">
                            
                               <label htmlFor="email">Email</label>
-                              <input type="email" value={this.state.email.val} onBlur={this.handleonFocus} onChange={this.handleChange}  name="email" />
+                              <input 
+                                type="email"
+                                value={this.state.email} 
+                                onBlur={this.validate} 
+                                onChange={this.handleChange}  
+                                name="email" />
+
                               {/* Error handeling  */}
                               {!this.state.emailError ? '' :  <span className="warning-box">{this.state.emailError}</span>}
 
                               <label htmlFor="username">Username</label>
-                              <input type="text" value={this.state.username.val} onBlur={this.handleonFocus} onChange={this.handleChange} name="username"  />
+                              <input 
+                                type="text" 
+                                value={this.state.username} 
+                                onBlur={this.validate} 
+                                onChange={this.handleChange} 
+                                name="username"  />
+
                               {/* Error handeling  */}
                               {!this.state.usernameError ? '' :  <span className="warning-box">{this.state.usernameError}</span>}
 
                               <label htmlFor="password">Password</label>
-                              <input type="password" value={this.state.password.val} onBlur={this.handleChange} onChange={this.handleChange}  maxLength="20" name="password" />
+                              <input 
+                                type="password" 
+                                value={this.state.password} 
+                                onBlur={this.validate} 
+                                onChange={this.handleChange}  
+                                maxLength="20" 
+                                name="password" />
+
                               {/* Error handeling  */}
                               {!this.state.passwordError ? '' :  <span className="warning-box">{this.state.passwordError}</span>}
 
